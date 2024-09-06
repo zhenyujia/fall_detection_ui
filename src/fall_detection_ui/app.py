3bf65b6c26eb0d47aed93639850de9b6c9230457
Vincent <vincejia@gmail.com>
2024-09-06 15:22:41 -0400

    add the first web cam app

---
+++ src/fall_detection_ui/app.py
@@ -0,0 +1,29 @@
+# https://towardsdatascience.com/developing-web-based-real-time-video-audio-processing-apps-quickly-with-streamlit-7c7bcd0bc5a8
+
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+import av
+import cv2
+
+st.title("My first Streamlit app")
+st.write("Hello, world")
+
+threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
+threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+
+
+def callback(frame):
+    img = frame.to_ndarray(format="bgr24")
+
+    img = cv2.cvtColor(cv2.Canny(img, threshold1, threshold2), cv2.COLOR_GRAY2BGR)
+
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_streamer(
+    key="example",
+    video_frame_callback=callback,
+    rtc_configuration={  # Add this line
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    }
+)
